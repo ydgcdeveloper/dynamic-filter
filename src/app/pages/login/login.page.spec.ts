ea2d4b6692c14d89889f44a8b6f553a4e511f22f
ydgcdeveloper <ydgcdeveloper@gmail.com>
2024-10-19 02:16:10 -0500

    Agregar HttpClientModule y configurar Store y Actions en las pruebas de LoginPage

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -1,11 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginPage } from './login.page';
+import { ScannedActionsSubject, Store } from '@ngrx/store';
+import { provideMockStore } from '@ngrx/store/testing';
+import { Actions } from '@ngrx/effects';
+import { Subject } from 'rxjs';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideMockStore({})
+      ]
+    });
+  });
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [Store, Actions, { provide: ScannedActionsSubject, useValue: new Subject() }],
+      imports: [HttpClientModule],
+    });
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
